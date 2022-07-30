9b1c641f949a077f79def16c466100270727cb09
LuisPulenta <luisalbertonu@gmail.com>
2022-07-30 18:56:05 -0300

    Icono y Splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Lands App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Lands App'),
+          title: const Text('Lands App'),
           centerTitle: true,
         ),
         body: Center(
           child: Container(
-            child: Text('Lands'),
+            child: const Text('Lands'),
           ),
         ),
       ),
